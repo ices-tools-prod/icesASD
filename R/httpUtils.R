d77d33ef14f19e80698867105b1038a3750fbd5c
Colin Millar <colin.millar@ices.dk>
2023-01-31 11:09:10 +0100

    add http utils

---
+++ R/httpUtils.R
@@ -0,0 +1,36 @@
+
+api_url <- function() {
+  "https://sg.ices.dk/adviceview/API"
+}
+
+#' @importFrom icesConnect ices_get_jwt
+#' @importFrom httr content
+ices_get <- function(url, retry = TRUE, quiet = FALSE, verbose = FALSE, content = TRUE, use_token = FALSE) {
+  resp <-
+    ices_get_jwt(
+      url,
+      retry = retry, quiet = quiet, verbose = verbose,
+      jwt = if (use_token) NULL else ""
+    )
+
+  if (content) {
+    content(resp, simplifyVector = TRUE)
+  } else {
+    resp
+  }
+}
+
+#' @importFrom icesConnect ices_post_jwt
+ices_post <- function(url, body = list(), retry = TRUE, verbose = FALSE, use_token = FALSE) {
+  out <-
+    ices_post_jwt(
+      url,
+      body,
+      encode = "multipart",
+      retry = retry,
+      verbose = verbose,
+      jwt = if (use_token) NULL else ""
+    )
+
+  return(out)
+}
