0122ee0471716f572e9c3be38edb69d538c3c61c
Luca Lamoni <lucalamoni@gmail.com>
2025-05-12 14:53:01 +0200

    add getASD function to retrieve and process multiple stock data - initial commit, cosidering approach

---
+++ R/getASD.R
@@ -0,0 +1,79 @@
+#' Get Any ASD Data
+#'
+#' This function combines the functionality of \code{\link{getListStocks}},
+#' \code{\link{getFishStockReferencePoints}}, \code{\link{getSummaryTable}}
+#' and \code{\link{getStockDownloadData}}.
+#' It supports querying many stocks and years in one function call.
+#'
+#' @param stock a stock name, e.g. cod-347d, or cod to find all cod stocks, or
+#'        NULL to process all stocks.
+#' @param year the assessment year, e.g. 2015, or 0 to process all years.
+#' @param data the data of interest, either "summary", "refpts" or "source".
+#' @param combine whether to combine the list output to a data frame.
+#' @param purpose the purpose of the entry, options are "Advice", "Bench",
+#'                "InitAdvice", default is "Advice".
+#'
+#' @note Only years with "Published" status are returned.
+#'
+#' @return A data frame (default) or a list if \code{combine} is \code{TRUE}.
+#'
+#' @seealso
+#' \code{\link{getListStocks}}, \code{\link{getSummaryTable}},
+#'   \code{\link{getFishStockReferencePoints}}, and
+#'   \code{\link{getStockDownloadData}} get a list of stocks, summary
+#'   results, reference points, and all data including custom columns.
+#'
+#' \code{\link{findAssessmentKey}} finds lookup keys.
+#'
+#' \code{\link{icesSAG-package}} gives an overview of the package.
+#'
+#' @author Colin Millar.
+#'
+#' @examples
+#' \dontrun{
+#' summary <- getSAG("had.27.46a20", 2022)
+#' refpts <- getSAG("had.27.46a20", 2022, "refpts")
+#'
+#' cod_summary <- getSAG("cod", 2022)
+#' cod_refpts <- getSAG("cod", 2015:2016, "refpts")
+#' cod_data <- getSAG("cod", 2017, "source-data")
+#' }
+#' @export
+
+getASD <- function(stock, year, assessmentKey = NULL, data = "table", combine = TRUE, purpose = "Advice") {
+  # select web service operation
+  data <- match.arg(data, c("table", "record", "notes"))
+  service <- switch(data,
+                    table = "getCatchScenariosTable",
+                    record = "getAdviceViewRecord",
+                    notes = "getCatchScenariosNotes")
+
+  # find lookup key
+  assessmentKey <- findAssessmentKey(stock, year, regex = TRUE, full = TRUE)
+  assessmentKey <- assessmentKey[assessmentKey$Purpose == purpose, "AssessmentKey"]
+
+  # get data requested by user
+  do.call(service, list(assessmentKey = assessmentKey))
+}
+
+out <- getCatchScenariosTable(adviceKey)
+
+  if (length(out) == 0) {
+    return(character(0))
+  }
+
+  out <-
+    pivot_wider(
+      out,
+      names_from = c(aK_ID, aK_Label, yearLabel, unit, stockDataType),
+      names_glue = "{aK_Label} ({yearLabel}) _{stockDataType}_",
+      values_from = value
+    )
+  
+  out <-
+    out[
+      !names(out) %in% c("assessmentKey", "adviceKey", "cS_Basis", "aR_ID")
+    ]
+  
+  cbind(Year = assessmentYear + 1, out)
+}
